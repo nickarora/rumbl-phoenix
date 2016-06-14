e62a78061273473bc39efece7a172eaab07fdb08
Nick Arora <arora.nick@gmail.com>
2016-06-14 11:18:10 -0700

    upgrade user_controller authenticate method to a function plug and apply to index and show

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,15 +1,12 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
+  plug :authenticate when action in [:index, :show]
+
   alias Rumbl.User
 
   def index(conn, _params) do
-    case authenticate(conn) do
-      %Plug.Conn{halted: true} = conn ->
-        conn
-      conn ->
-        users = Repo.all(User)
-        render conn, "index.html", users: users
-    end
+    users = Repo.all(User)
+    render conn, "index.html", users: users
   end
 
   def new(conn, _params) do
@@ -35,7 +32,7 @@ defmodule Rumbl.UserController do
     end
   end
 
-  defp authenticate(conn) do
+  defp authenticate(conn, _opts) do
     if conn.assigns.current_user do
       conn
     else
