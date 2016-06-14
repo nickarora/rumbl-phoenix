a78628b90608eaa31f054a52064d2c443943cfa0
Nick Arora <arora.nick@gmail.com>
2016-06-14 11:02:42 -0700

    create authentication plug

---
+++ web/controllers/auth.ex
@@ -0,0 +1,16 @@
+defmodule Rumbl.Auth do
+  import Plug.Conn
+
+  def init(opts) do
+    # extract the repo from options
+    Keyword.fetch!(opts, :repo)
+  end
+
+  # call receives repo from init
+  def call(conn, repo) do
+    # if user_id exists in the session, look it up and assign it within conn
+    user_id = get_session(conn, :user_id)
+    user    = user_id && repo.get(Rumbl.User, user_id)
+    assign(conn, :current_user, user)
+  end
+end
