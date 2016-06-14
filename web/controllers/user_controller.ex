6ca4c8c28ae8aef819b7474d2d64a4ab342682dd
Nick Arora <arora.nick@gmail.com>
2016-06-13 23:20:47 -0700

    create form to create new users

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -7,8 +7,22 @@ defmodule Rumbl.UserController do
     render conn, "index.html", users: users
   end
 
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render conn, "new.html", changeset: changeset
+  end
+
   def show(conn, %{"id" => id}) do
     user = Repo.get(User, id)
     render conn, "show.html", user: user
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+    {:ok, user} = Repo.insert(changeset)
+
+    conn
+    |> put_flash(:info, "#{user.name} created!")
+    |> redirect(to: user_path(conn, :index))
   end
 end
