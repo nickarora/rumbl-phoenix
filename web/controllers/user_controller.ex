37f43599fd8c7391138b74c4e73185c72e40db6d
Nick Arora <arora.nick@gmail.com>
2016-06-13 21:41:37 -0700

    switch to a postgres db using ecto

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -2,12 +2,14 @@ defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
 
   def index(conn, _params) do
-    users = Repo.all(Rumbl.User)
+    # users = Repo.all(Rumbl.User)
+    users = []
     render conn, "index.html", users: users
   end
 
   def show(conn, %{"id" => id}) do
-    user = Repo.get(Rumbl.User, id)
+    # user = Repo.get(Rumbl.User, id)
+    user = %{id: "1", name: "José", username: "josevalim", password: "elixir"}
     render conn, "show.html", user: user
   end
 end
